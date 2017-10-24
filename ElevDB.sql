7cd0323b3e0f82139e117dd5f3be1d10f2129718
Tordenilden <33028544+Tordenilden@users.noreply.github.com>
2017-10-24 15:22:46 +0200

    Update ElevDB.sql

--- ElevDB.sql
+++ ElevDB.sql
@@ -1,4 +1,4 @@
---drop database ElevDB
+----drop database ElevDB
 --go
 --create database ElevDB
 --go
@@ -13,7 +13,9 @@
 --select * from ElevDB.INFORMATION_SCHEMA.TABLES
 --select * from sys.tables
 --drop database ElevDB
-
+--her er der noget fra flemse
+select * from sys.tables
+-- flemse slut
 if not exists(select * from sys.Databases where name = 'ElevDB')
 begin
    create database ElevDB
@@ -36,11 +38,11 @@ begin
 end 
 go
 
-drop table if exists Fag_Lærer
+drop table if exists Fag_LÃ¦rer
 go
 drop table if exists Fag
 go
-drop table if exists Lærer
+drop table if exists LÃ¦rer
 go
 
 
@@ -53,7 +55,7 @@ create table Fag
 create table dbo.Elev
 (
    Id int Primary Key identity,
-   Navn Nvarchar(35) not null check(Navn not like '%[^A-Å a-å]%'),
+   Navn Nvarchar(35) not null check(Navn not like '%[^A-Ã… a-Ã¥]%'),
    Kendetegn Nvarchar(30),
    Alder int check(Alder between 0 and 75),
    Placering decimal(2,1),
@@ -61,17 +63,17 @@ create table dbo.Elev
    FagId int foreign key references Fag(Id) --not null
 )
 
-create table Lærer
+create table LÃ¦rer
 (
    Id int primary key,
    Navn nvarchar(30)
 )
 
-create table Fag_Lærer
+create table Fag_LÃ¦rer
 (
    FagId int foreign key references Fag(Id),
-   LærerId int foreign key references Lærer(Id),
-   constraint UK_FagLærer Unique(FagId, LærerId)
+   LÃ¦rerId int foreign key references LÃ¦rer(Id),
+   constraint UK_FagLÃ¦rer Unique(FagId, LÃ¦rerId)
 )
 
 insert into Fag values('Engelskt')
@@ -82,44 +84,44 @@ insert into Fag values('Gymnastikt')
 insert into Fag values('Lokumsrensningt')
 insert into Fag values('Sporvognsskinneskidtskraber')
 
-insert into Elev values('Alex Skram Jensen', 'Hår og skæg', 26, 2.5,'Skab 3 PC 10', 1002)
+insert into Elev values('Alex Skram Jensen', 'HÃ¥r og skÃ¦g', 26, 2.5,'Skab 3 PC 10', 1002)
 insert into Elev values('Balen Hassan Dezai', 'The first one', 21, 1.1, 'Skab 1 PC 2', 1000)
 insert into Elev values
 ('Belawel Nadeem Sharif', 'Flot fyr', 23, 4.2, 'Skab 1 PC 10', 1003),
 ('Birk Blauner Clausen', 'Polo Kasket', 23, 1.5,'Skab 1 PC 8', 1002),    
-('Casper Hilligsøe', 'Granvoksen fyr', 75, 1.4, 'Skab 4 PC 8', 1005),
+('Casper HilligsÃ¸e', 'Granvoksen fyr', 75, 1.4, 'Skab 4 PC 8', 1005),
 ('Frederik Christensen', 'Omvendt sort kasket', 24, 3.4, 'Egen PC', 1004),
-('Henrik Vincents Poulsen', 'To børn', 41, 1.3, 'Skab 1 PC 4', 1000),
+('Henrik Vincents Poulsen', 'To bÃ¸rn', 41, 1.3, 'Skab 1 PC 4', 1000),
 ('Jacob Hytting', 'Kontroversiel', 19, 3.1, 'Skab 3 PC 8', 1000),
-('Jason Timothy Tolentino Sabeniano', 'Altid træt', 20, 2.4, 'Skab 3 PC 5', 1004),
+('Jason Timothy Tolentino Sabeniano', 'Altid trÃ¦t', 20, 2.4, 'Skab 3 PC 5', 1004),
 ('Jesper Hvid Vestergaard', 'Snakker meget', 29, 3.3, 'Skab 1 PC 7', 1002),
-('Jim Kleckers', 'Lidt skæg', 25, 4.1, 'Skab 3 PC 2', 1000),
-('Kevin Jan Kjærgaard Winther', 'Endnu mere lidt (fip)skæg', 26, 3.2, 'Skab 1 PC 6', 1001),
+('Jim Kleckers', 'Lidt skÃ¦g', 25, 4.1, 'Skab 3 PC 2', 1000),
+('Kevin Jan KjÃ¦rgaard Winther', 'Endnu mere lidt (fip)skÃ¦g', 26, 3.2, 'Skab 1 PC 6', 1001),
 ('Mikkel Frydenlund Hammer', 'Second to one', 21, 1.2, 'Skab 1 PC 3', 1005),
-('Morten Kjær', 'Normale fingre', 21, 3.6, 'Egen PC', 1003),
+('Morten KjÃ¦r', 'Normale fingre', 21, 3.6, 'Egen PC', 1003),
 ('Nicolai Jensen', 'The curling wiking', 18, 2.3, 'Skab 3 PC 3', 1001),
 ('Patrick Lolk', 'Hvid kasket', 21, 2.6, 'Skab 1 PC 9', 1005),
-('Philip Østergaard Klein', 'Har (børne)kørekort', 17, 2.1, 'Skab 4 PC 2', 1003),
-('Rasmus Kanstrup Poulsen', 'Intet kørekort mørkebrunt skæg', 20, 4.4, 'Skab 3 PC 7', 1005),
-('Rasmus Kock Rask', 'Rød og Rask', 23, 4.3, 'Skab 3 PC 4', 1005),
+('Philip Ã˜stergaard Klein', 'Har (bÃ¸rne)kÃ¸rekort', 17, 2.1, 'Skab 4 PC 2', 1003),
+('Rasmus Kanstrup Poulsen', 'Intet kÃ¸rekort mÃ¸rkebrunt skÃ¦g', 20, 4.4, 'Skab 3 PC 7', 1005),
+('Rasmus Kock Rask', 'RÃ¸d og Rask', 23, 4.3, 'Skab 3 PC 4', 1005),
 ('Simon Johnsen', 'Tror han er gud', 20, 2.2, 'Skab 3 PC 6', 1000),
-('Søren Vollmer Riishede', 'Ornli syg', 19, 4.6, 'Skab 3 PC 9', 1005),
-('Virtus Brædder Opstrup', 'Sidst på listen', 18, 3.5, 'Egen PC', null)
+('SÃ¸ren Vollmer Riishede', 'Ornli syg', 19, 4.6, 'Skab 3 PC 9', 1005),
+('Virtus BrÃ¦dder Opstrup', 'Sidst pÃ¥ listen', 18, 3.5, 'Egen PC', null)
 
 
-insert into Lærer values(777, 'Jan Stern Johansen')
-insert into Lærer values(778, 'Flemming Sørensen')
-insert into Lærer values(779, 'John Ellehammer')
-insert into Lærer values(780, 'Bo Steffensen')
+insert into LÃ¦rer values(777, 'Jan Stern Johansen')
+insert into LÃ¦rer values(778, 'Flemming SÃ¸rensen')
+insert into LÃ¦rer values(779, 'John Ellehammer')
+insert into LÃ¦rer values(780, 'Bo Steffensen')
 
-insert into Fag_Lærer values(1006, 777)
-insert into Fag_Lærer values(1005, 778)
-insert into Fag_Lærer values(1004, 779)
-insert into Fag_Lærer values(1003, 780)
-insert into Fag_Lærer values(1002, 780)
-insert into Fag_Lærer values(1001, 780)
-insert into Fag_Lærer values(1000, 780)
---insert into Fag_Lærer values(1000, 780)
+insert into Fag_LÃ¦rer values(1006, 777)
+insert into Fag_LÃ¦rer values(1005, 778)
+insert into Fag_LÃ¦rer values(1004, 779)
+insert into Fag_LÃ¦rer values(1003, 780)
+insert into Fag_LÃ¦rer values(1002, 780)
+insert into Fag_LÃ¦rer values(1001, 780)
+insert into Fag_LÃ¦rer values(1000, 780)
+--insert into Fag_LÃ¦rer values(1000, 780)
 
 if(null=null)
 begin
@@ -165,10 +167,10 @@ full join Fag on Elev.FagId = Fag.Id
 select * from Elev
 full join Fag on Elev.FagId = Fag.Id
 
-select * from Elev, Fag, Fag_Lærer, Lærer
+select * from Elev, Fag, Fag_LÃ¦rer, LÃ¦rer
 where Elev.FagId = Fag.Id
-and Fag.Id = Fag_Lærer.FagId
-and Lærer.Id = Fag_Lærer.LærerId
+and Fag.Id = Fag_LÃ¦rer.FagId
+and LÃ¦rer.Id = Fag_LÃ¦rer.LÃ¦rerId
 
 go
 
@@ -177,17 +179,17 @@ go
 
 create view vwElevFag
 as
-select Elev.Navn as Elevnavn, Fag.Navn as 'Fagets Navn', Lærer.Navn as [Lærers Navn] from Elev, Fag, Fag_Lærer, Lærer
+select Elev.Navn as Elevnavn, Fag.Navn as 'Fagets Navn', LÃ¦rer.Navn as [LÃ¦rers Navn] from Elev, Fag, Fag_LÃ¦rer, LÃ¦rer
 where Elev.FagId = Fag.Id
-and Fag.Id = Fag_Lærer.FagId
-and Lærer.Id = Fag_Lærer.LærerId
+and Fag.Id = Fag_LÃ¦rer.FagId
+and LÃ¦rer.Id = Fag_LÃ¦rer.LÃ¦rerId
 go
 
 select * from vwElevFag
 where [Fagets Navn] like 'E%k_'
 go
 
---Går galt, flere kolonner med samme navn
+--GÃ¥r galt, flere kolonner med samme navn
 --create view vwAllElevFag
 --as
 --select * from Elev, Fag
